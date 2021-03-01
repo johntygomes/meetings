e7c0dd63690fea60fa733c18898744ef9c953b9c
johntygomes <32851005+johntygomes@users.noreply.github.com>
2021-03-01 19:42:41 +0530

    second commit

--- meetingmanagement/settings.py
+++ meetingmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!mfh8&stth&545$p0og8+q76qo(^e4if_ppn5xdt0&f%be1*cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['meetingmanagementjohnty.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
